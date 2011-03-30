99bac54140472fa9b7f9ed76e0b20cd4b035158d
Steve Spencer <steve@bigfrog.net>
2011-03-29 22:36:49 -0700

    add text

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("http").createServer(function (req, res) {
   res.writeHead(200, {})
-  res.end("Hello, world!")
+  res.end("Hello, world! Add a fun api or something.")
 }).listen(80)
 console.log("waiting to say hello.")
